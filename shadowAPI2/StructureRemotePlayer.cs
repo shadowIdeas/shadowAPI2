e765665a8d8dc28751a8430dc7266f8f5464540e
shadowlif <shadowlifDev@outlook.com>
2014-07-06 15:59:55 +0200

    Added StructureRemotePlayer

---
+++ shadowAPI2/StructureRemotePlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace shadowAPI2
+{
+    struct StructureRemotePlayer
+    {
+        public uint id { get; set; }
+        public int score { get; set; }
+        public int ping { get; set; }
+        public string name { get; set; }
+    }
+}
