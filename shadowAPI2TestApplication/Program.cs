c3c34a49cd6e38a5e58333c2005ec4d980296077
shadowlif <shadowlifDev@outlook.com>
2014-07-11 20:22:18 +0200

    Added id example

--- shadowAPI2TestApplication/Program.cs
+++ shadowAPI2TestApplication/Program.cs
@@ -20,23 +20,24 @@ namespace shadowAPI2TestApplication
             System.Threading.Thread.Sleep(5000);
 
             Console.WriteLine("Sending Test-Message (UTF-8 Symbols included)...");
-            shadowAPI2.Chat.GetInstance().Send("UTF8: äüéö <= Gehts? :>");            
+            shadowAPI2.Chat.GetInstance().Send("UTF8: äüéö <= Gehts? :>");
+            shadowAPI2.Chat.GetInstance().AddMessage("My ID: " + shadowAPI2.Player.GetInstance().GetId());
 
             int id = -1;
             String name = "";
-            while(id == -1)
+            while (id == -1)
             {
                 Console.Write("Which player-name should be queried: ");
                 name = Console.ReadLine();
-                id = shadowAPI2.RemotePlayer.GetInstance().GetPlayerIdByName(name,true);
+                id = shadowAPI2.RemotePlayer.GetInstance().GetPlayerIdByName(name, true);
                 if (id == -1)
                     Console.WriteLine("Player '" + name + "' not found");
             }
             Console.WriteLine("Player '" + name + "' has the ID: " + id);
 
             Console.WriteLine("Testing AddChatMessage...");
-            shadowAPI2.Chat.GetInstance().AddMessage("ID von " + name + ": " + id,Color.Yellow);
-            shadowAPI2.Chat.GetInstance().AddMessage("Name von ID " + id + ": " + shadowAPI2.RemotePlayer.GetInstance().GetPlayerNameById((uint)id),Color.Turquoise);
+            shadowAPI2.Chat.GetInstance().AddMessage("ID von " + name + ": " + id, Color.Yellow);
+            shadowAPI2.Chat.GetInstance().AddMessage("Name von ID " + id + ": " + shadowAPI2.RemotePlayer.GetInstance().GetPlayerNameById((uint)id), Color.Turquoise);
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Test completed [You can go into SA:MP and check your Chat-Messages]");
