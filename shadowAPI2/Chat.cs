c9cc0112a90f105eaacef6ac7efc41834c01de6d
Johnny @PC <johnny.96@gmx.de>
2014-07-12 19:18:02 +0200

    Added Chat-Message Event

--- shadowAPI2/Chat.cs
+++ shadowAPI2/Chat.cs
@@ -4,16 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.IO;
 
 namespace shadowAPI2
 {
     public class Chat
     {
         private static Chat instance;
+        private FileSystemWatcher watcher;
+        private String ChatlogPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "GTA San Andreas User Files\\SAMP\\");
+        private String ChatlogFile = "chatlog.txt";
+
+        /// <summary>
+        /// Chat-Message delegate
+        /// </summary>
+        /// <param name="time">When the message was sent</param>
+        /// <param name="message">The message itself</param>
+        public delegate void OnChatMessageReceived(DateTime time, String message);
+
+        /// <summary>
+        /// This Event will provide you with the latest chat-messages
+        /// </summary>
+        public event OnChatMessageReceived OnChatMessage;
 
         private Chat()
         {
-
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = ChatlogPath;
+            watcher.Filter = ChatlogFile;
+            watcher.Changed += ChangeReceived;
+            watcher.EnableRaisingEvents = true;
+        }
+        internal void ChangeReceived(object ob, FileSystemEventArgs e)
+        {
+            using (StreamReader reader = new StreamReader(new FileStream(Path.Combine(ChatlogPath, ChatlogFile), FileMode.Open, FileAccess.Read)))
+            {
+                String line = "";
+                String lastline = "";
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                    if (line != "")
+                        lastline = line;
+                }
+                if (lastline == "")
+                    return;
+                String[] splitted = lastline.Split(' ');
+                DateTime date = DateTime.Parse(splitted[0].Remove(0, 1).Remove(splitted[0].Length - 2));
+                splitted[0] = "";
+                if (OnChatMessage != null)
+                    OnChatMessage(date, string.Join(" ", splitted));
+            }
         }
 
         public static Chat GetInstance()
@@ -95,7 +136,7 @@ namespace shadowAPI2
             if (!Memory.IsInit)
                 Memory.Init();
 
-            Memory.Call(Memory.functionAddChatMessage, new object[] {(int)Memory.chatMessage, "{" + color + "}" + text }, true);
+            Memory.Call(Memory.functionAddChatMessage, new object[] { (int)Memory.chatMessage, "{" + color + "}" + text }, true);
         }
 
         /// <summary>
